dfa1fdc9cd6532b4f97bc0905a54fcee09e33510
Oren Novotny <oren@novotny.org>
2013-06-06 09:31:50 -0400

    Add project to debug native portion

---
+++ PhoneAppWithTimeZones/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneAppWithTimeZones.Resources;
+
+namespace PhoneAppWithTimeZones
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
